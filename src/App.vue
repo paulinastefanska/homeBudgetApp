fd6716f7b4dc28408baa4218cd4f39998b5c2ebd
Paulina Stefanska <web.paulinastefanska@gmail.com>
2020-11-07 16:14:32 +0100

    Added tailwind css

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div
+    id="app"
+    class="container mx-auto"
+  >
+    <HelloWorld />
   </div>
 </template>
 
@@ -17,12 +19,4 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    fontFamily: {
+      display: ['Open Sans', 'sans-serif'],
+      body: ['Open Sans', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        black: '#151515',
+      },
+      margin: {
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
